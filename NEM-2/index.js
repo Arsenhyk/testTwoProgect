2ddfb6d8b5ff24cbbb6a67a5e673ec79d014db3f
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-01-03 23:41:37 +0200

    add bodyParser

--- NEM-2/index.js
+++ NEM-2/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var bodyParser = require('body-parser')
+
+//указываем  express использовать bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 //указываем  express использовать  ejs
 app.set('view engine', 'ejs')
 
@@ -10,10 +16,17 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
 })
+
 //рендеринг create страници 
 app.get('/create', (req, res) => {
   res.render('create')
 })
+
+//обработчик create страници 
+app.post('/create', (req, res) => {
+  console.log(req.body);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
