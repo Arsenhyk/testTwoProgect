506ac36a0ad8c52fcfbfc146caa85c39fda83211
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-01-03 22:47:11 +0200

    add create.ejs

--- NEM-2/index.js
+++ NEM-2/index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+//рендеринг  страници create
+app.get('/create', (req, res) => {
+  res.render('create')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 }) 
