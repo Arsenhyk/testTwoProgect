32f27aafc7385839d73128b0bfda6d3a12b22236
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-01-03 22:38:28 +0200

    add index.ejs

--- NEM-2/index.js
+++ NEM-2/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//указываем  express использовать  ejs
+app.set('view engine', 'ejs')
+
+
+//рендеринг главной страници 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!')
+  res.render('index')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+}) 
