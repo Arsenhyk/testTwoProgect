8edd3417f94273ae29dc9c0bab49fe61921ae27b
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-01-08 23:29:57 +0200

    delet old project

--- NEM-2/app.js
+++ NEM-2/app.js
@@ -4,8 +4,7 @@ const app = express()
 const path = require('path');
 
 
-//подключаем post
-const Post = require('./models/post');
+
 
 //bodyParser
 var bodyParser = require('body-parser')
@@ -29,36 +28,9 @@ app.set('view engine', 'ejs')
 //рендеринг главной страници 
 app.get('/', (req, res) =>
  //обьекты рендеренга на главную страницу
-{
-  Post.find({}).then(posts =>{
-    res.render('index', {posts: posts})
-  })
-})
-/*  {
-  res.render('index', {arr: arr})
-}) */
 
-//рендеринг create страници 
-app.get('/create', (req, res) => {
-  res.render('create')
-})
-
-//обработчик create страници 
-app.post('/create', (req, res) => {
-
-  //заносим в БД
-  const{title, body} = req.body; //рестрктирезация 
-
-  Post.create({
-    title: title,
-    body: body
-  }).then(post => console.log(post.id))
-
-  //выводим на страницу
-  //arr.push(req.body.title)
-
-  res.redirect('/')
-  console.log(req.body);
+ {
+  res.render('index')
 })
 
 module.exports = app
