9963da8172c9ba9e2f1b62b056b12c86fc1d1361
arsenhuk_oleh <aresnhykoleh@gmail.com>
2021-01-18 22:16:08 +0200

    add scss

---
+++ NEM-2/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const plumber = require('gulp-plumber');
+
+
+// задача gulp sass
+gulp.task('sass', function(done) {
+    gulp.src('dev/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.stream())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'],{
+            cascade: true
+        }))
+        .pipe(cssnano())
+
+    done();
+});
+
+// задача gulp serve
+gulp.task('serve', function(done) {
+
+    browserSync.init({
+        server: 'dist'
+    });
+
+    gulp.watch('dev/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('dist/*.html').on('change', () => {
+      browserSync.reload();
+      done();
+    });
+  
+
+    done();
+});
+//оновная задача gulp
+gulp.task('default', gulp.series('sass', 'serve'));
